fcb0b18b768d606903d9eab24d5a7e9ba2742a87
alexsevas <a1exsevas@yandex.ru>
2025-10-27 21:27:14 +0300

    Add main.py v0.1.0

---
+++ main.py
@@ -0,0 +1,22 @@
+# v0.1.0
+
+# conda activate allpy311
+# pip install PyQt5 PyQtWebEngine markdown chardet Pygments
+
+
+# Подавление предупреждений о deprecated sipPyTypeDict
+import warnings
+import sys
+
+warnings.filterwarnings("ignore", category=DeprecationWarning, message=".*sipPyTypeDict.*")
+
+from PyQt5.QtWidgets import QApplication
+from editor import MarkdownEditor
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    editor = MarkdownEditor(app)
+    editor.show()
+
+    sys.exit(app.exec_())
